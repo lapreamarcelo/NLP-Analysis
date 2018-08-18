b30c068b4e751aa17e6caaa4949d0e6a3218bc65
Marcelo Laprea <30522943+marcelolaprea@users.noreply.github.com>
2018-08-18 14:00:46 +0200

    Last commit

--- NLP.py
+++ NLP.py
@@ -16,48 +16,112 @@ from pos.pos_stanford import pos_tagger as STANFORD_Tagger
 from grounding.stemmer import stemmer
 from grounding.lemmatizer import lemmatizer
 import words_extraction
+import time
 
 """
 Conexion con la Base de Datos y creacion de DataFrame
 """
-mySQLConnection = MySQL.MySQLConnection("localhost", "user", "pass", "noticias")
+mySQLConnection = MySQL.MySQLConnection("localhost", "marcelo", "pass", "noticias")
 df = mySQLConnection.createDataFrame()
+
+"""
+Frecuencia de las diferentes palabras
+"""
+
+def returnWords(dataFrame): 
+    words = []
+    
+    for word in dataFrame['Title']:
+        word = word.split()
+        words += (word)
+    
+    return words
+
+start_time = time.time()
+words = returnWords(df)
+end_time = time.time() - start_time
+
+wordsFreq = words_extraction.Words_Extraction().returnWordFrequency(words, 20)
+
 
 """
 Analisis POS con NLTK
 """
+start_time = time.time()
 tokens = NLTK_Tagger.NLTK_POS_Tagger().returnTokens(df)
+
+
 stemmerTokens = stemmer.Stemmer_Grounding().returnPorterStemmerTokens(tokens)
 lemmatizerTokens = lemmatizer.Lemmatizer_Grounding().returnTokens(tokens)
 
 tagged = NLTK_Tagger.NLTK_POS_Tagger().returnTaggedWords(tokens)
 
-verbs = words_extraction.Words_Extraction().detect_verbs(tokens)
-nouns = words_extraction.Words_Extraction().detect_nouns(tokens)
-adjectives = words_extraction.Words_Extraction().detect_adjectives(tokens)
+verbs = words_extraction.Words_Extraction().detect_verbs(stemmerTokens)
+nouns = words_extraction.Words_Extraction().detect_nouns(stemmerTokens)
+adjectives = words_extraction.Words_Extraction().detect_adjectives(stemmerTokens)
 
-nounsFreq = words_extraction.Words_Extraction().returnWordFrequency(nouns, 10)
-adjectivesFreq = words_extraction.Words_Extraction().returnWordFrequency(adjectives, 10)
-verbsFreq = words_extraction.Words_Extraction().returnWordFrequency(verbs, 10)
+nounsFreq = words_extraction.Words_Extraction().returnWordFrequency(nouns, 20)
+adjectivesFreq = words_extraction.Words_Extraction().returnWordFrequency(adjectives, 20)
+verbsFreq = words_extraction.Words_Extraction().returnWordFrequency(verbs, 20)
+end_time = time.time() - start_time
 
 """
 Analisis POS con SPACY
 """
+#Sin grounding
+start_time = time.time()
 tokens = SPACY_Tagger.SPACY_POS_Tagger.returnSpacy(df)
+end_time = time.time() - start_time
+
+stemmerTokens = stemmer.Stemmer_Grounding().returnPorterStemmerTokens(tokens)
+
+#Con Lemma
+start_time = time.time()
+tokens = SPACY_Tagger.SPACY_POS_Tagger.returnLemmaSpacy(df)
+end_time = time.time() - start_time
 
 verbs = words_extraction.Words_Extraction().detect_verbs(tokens)
 nouns = words_extraction.Words_Extraction().detect_nouns(tokens)
 adjectives = words_extraction.Words_Extraction().detect_adjectives(tokens)
 
-nounsFreq = words_extraction.Words_Extraction().returnWordFrequency(nouns, 10)
-adjectivesFreq = words_extraction.Words_Extraction().returnWordFrequency(adjectives, 10)
-verbsFreq = words_extraction.Words_Extraction().returnWordFrequency(verbs, 10)
+nounsFreq = words_extraction.Words_Extraction().returnWordFrequency(nouns, 20)
+adjectivesFreq = words_extraction.Words_Extraction().returnWordFrequency(adjectives, 20)
+verbsFreq = words_extraction.Words_Extraction().returnWordFrequency(verbs, 20)
 
 """
 Analisis POS con STANFORD
 """
-tokens = STANFORD_Tagger.POS_tagger().returnTokens(df)
+from nltk.corpus import stopwords
+import string
 
+start_time = time.time()
+tokens = STANFORD_Tagger.POS_tagger().returnTokens(df)
+end_time = time.time() - start_time
+
+stanfordTokens = [word[0] for word in tokens]
+
+#Cleaning
+stanfordTokens = [word.lower() for word in stanfordTokens]
+cleanPunctuation = str.maketrans('','', string.punctuation)
+stanfordTokens = [word.translate(cleanPunctuation) for word in stanfordTokens]
+stanfordTokens = [word for word in stanfordTokens if word.isalpha()]
+stop_words = set(stopwords.words('spanish'))
+stanfordTokens = [word for word in stanfordTokens if not word in stop_words]
+
+
+stemmerTokens = stemmer.Stemmer_Grounding().returnPorterStemmerTokens(stanfordTokens)
+lemmatizerTokens = lemmatizer.Lemmatizer_Grounding().returnTokens(stanfordTokens)
+
+#Sin Grounding
 verbs = STANFORD_Tagger.POS_tagger().detect_verbs(tokens)
 nouns = STANFORD_Tagger.POS_tagger().detect_nouns(tokens)
 adjectives = STANFORD_Tagger.POS_tagger().detect_adjectives(tokens)
+
+#Con Grounding
+verbs = words_extraction.Words_Extraction().detect_verbs(stemmerTokens)
+nouns = words_extraction.Words_Extraction().detect_nouns(stemmerTokens)
+adjectives = words_extraction.Words_Extraction().detect_adjectives(stemmerTokens)
+
+nounsFreq = words_extraction.Words_Extraction().returnWordFrequency(nouns, 20)
+adjectivesFreq = words_extraction.Words_Extraction().returnWordFrequency(adjectives, 20)
+verbsFreq = words_extraction.Words_Extraction().returnWordFrequency(verbs, 20)
--- pos/pos_spacy/spacy_pos_tagger.py
+++ pos/pos_spacy/spacy_pos_tagger.py
@@ -12,7 +12,7 @@ import string
 
 class SPACY_POS_Tagger:
     
-    def returnSpacy(dataFrame):
+    def returnLemmaSpacy(dataFrame):
         nlp = spacy.load('es_core_news_sm')
         tokens = []
         
@@ -21,6 +21,30 @@ class SPACY_POS_Tagger:
             
             for token in doc:
                 posToken = token.lemma_
+                posToken = posToken.lower()
+                
+                #Quitar signos de puntuacion
+                cleanPunctuation = str.maketrans('','', string.punctuation)
+                posToken = posToken.translate(cleanPunctuation)
+                
+                #Eliminamos los stopwords
+                stop_words = set(stopwords.words('spanish'))
+                
+                if posToken.isalpha() and posToken not in stop_words:
+                    tokens.append(posToken)
+                
+        return tokens
+    
+    
+    def returnSpacy(dataFrame):
+        nlp = spacy.load('es_core_news_sm')
+        tokens = []
+        
+        for sentence in dataFrame['Title']:
+            doc = nlp(sentence)
+            
+            for token in doc:
+                posToken = token.text
                 posToken = posToken.lower()
                 
                 #Quitar signos de puntuacion
