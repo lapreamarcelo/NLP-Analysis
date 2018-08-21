5ab282e31ceba26da3c0dd72148b8e406e518971
Marcelo Laprea <30522943+marcelolaprea@users.noreply.github.com>
2018-08-21 17:48:25 +0200

    Cleaning NER code

--- ner/ner_spacy/spacy_ner.py
+++ ner/ner_spacy/spacy_ner.py
@@ -8,47 +8,64 @@ Created on Sat Aug 18 22:04:19 2018
 import spacy
 from collections import Counter
 
-class SPACY_NER:
+class spacy_ner:
 
-    def getArticles(self, dataFrame):
+    def get_articles(self, data_frame):
         articles = []
         nlp = spacy.load('es_core_news_sm')
         
-        for sentence in dataFrame['Title']:
+        for sentence in data_frame['Title']:
             doc = nlp(sentence)
             articles.append(doc)
             
         return articles
     
     
-    def getEntities(self, articles):
+    def get_entities(self, articles):
         entities = []
         
         for article in articles:
             
-            for X in article.ents:
-                entities.append({X.label_: X.text})
+            for entity in article.ents:
+                entities.append({entity.label_: entity.text})
                 
         return entities
     
     
-    def countEntities(self, articles):
+    def count_entities(self, articles):
         labels = []
         
         for article in articles:
             
-            for x in article.ents:
-                labels.append(x.label_)
+            for entity in article.ents:
+                labels.append(entity.label_)
                 
         return Counter(labels)
     
     
-    def mostCommons(self, articles, mostCommon):
+    def most_commons(self, articles, most_common):
         items = []
         
         for article in articles:
             
-            for x in article.ents:
-                items.append(x.text)
+            for entity in article.ents:
+                items.append(entity.text)
         
-        return Counter(items).most_common(mostCommon)
+        return Counter(items).most_common(most_common)
+
+
+
+def getArticles(dataFrame):
+        articles = []
+        nlp = spacy.load('es_core_news_sm')
+        
+        for sentence in dataFrame['Title']:
+            article = ""
+            
+            doc = nlp(sentence)
+            articles.append(doc)
+            
+        return articles
+    
+    
+articles = getArticles(df)
